b1f2d0581cc261c8dd2d9196e44455dda5e26548
cesarbonadio <cesarbonadio123@gmail.com>
2020-05-23 12:26:01 -0700

    added second highest salary sql

---
+++ second-highest-salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select max(salary) as SecondHighestSalary 
+from Employee
+where salary not in
+(
+    select max(salary) from Employee order by salary desc
+) 
